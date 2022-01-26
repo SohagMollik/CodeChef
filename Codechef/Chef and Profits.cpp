7e4ec0c81146ce8e659c5721063a8d30b3016655
Sohag Mollik <sohagmollik871@gmail.com>
2022-01-27 01:22:00 +0600

    Add files via upload

---
+++ Codechef/Chef and Profits.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ int t;
+ cin>>t;
+ while(t--){
+ int x,y,z;
+ cin>>x>>y>>z;
+ cout<<(x*z)-(x*y)<<"\n";
+ }
+    return 0;
+}
