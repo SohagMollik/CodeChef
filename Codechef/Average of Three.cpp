7e770a10070e8b679e136c155a9a713c0fe92a45
Sohag Mollik <sohagmollik871@gmail.com>
2022-01-27 00:40:38 +0600

    Add files via upload

---
+++ Codechef/Average of Three.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ int t;
+ cin>>t;
+ while(t--){
+ int x;
+ cin>>x;
+ cout<<1<<" "<<2<<" "<<(3*x)-3<<"\n";
+ }
+    return 0;
+}
+
